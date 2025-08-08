ac80fffa134cb4d3a310681408e5434c9311f936
txy-nju <2431116525@qq.com>
2025-08-08 09:32:24 +0800

    添加了volumn组件用于快速展示调研成果

--- practiceui/src/Pages/HomePages/homePage.jsx
+++ practiceui/src/Pages/HomePages/homePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './header';
 import RollingDisplay from './rollingDisplay';
+import Volumn from './volumn';
 import './homePage.css'; // 确保引入样式文件
 
 function HomePage() {
@@ -8,6 +9,7 @@ function HomePage() {
         <div className="homepage">
             <Header />
             <RollingDisplay />
+            <Volumn />
             <main className="main-content">
                 <h2>欢迎来到首页</h2>
                 <p>这里是首页的内容</p>
