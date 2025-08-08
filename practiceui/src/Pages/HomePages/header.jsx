a6efc90e9a919fcea7dc5e4f02c80dea0ef2353f
txy-nju <2431116525@qq.com>
2025-08-08 22:15:15 +0800

    完成了资源夹的分类，分为大文件存放和小文件存放两部分

--- practiceui/src/Pages/HomePages/header.jsx
+++ practiceui/src/Pages/HomePages/header.jsx
@@ -4,7 +4,8 @@ import './header.css';
 function Header() {
     return (
         <header className="header">
-            <h1>Header with Gradient Background</h1>
+            <h1>东北红色文化</h1>
+            {/* 或者其他红色文化相关的标题 */}
         </header>
     );
 }
